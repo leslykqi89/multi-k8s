f116d7dca342a3e3a411233d4dcc486aa0149dca
Lesly Quiñonez <leslykqi@gmail.com>
2020-07-14 22:23:07 -0600

    Fixed issue with docker username

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
-docker build -t leslykqi89/multi-client:latest -t leslykqi89/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t leslykqi89/multi-server:latest -t leslykqi89/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t leslykqi89/multi-worker:latest -t leslykqi89/multi-worker:$SHA -f ./worker/Dockerfile ./worker
-docker push leslykqi89/multi-client:latest
-docker push leslykqi89/multi-server:latest
-docker push leslykqi89/multi-worker:latest
-docker push leslykqi89/multi-client:$SHA
-docker push leslykqi89/multi-server:$SHA
-docker push leslykqi89/multi-worker:$SHA
+docker build -t leslykqi/multi-client:latest -t leslykqi/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t leslykqi/multi-server:latest -t leslykqi/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t leslykqi/multi-worker:latest -t leslykqi/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push leslykqi/multi-client:latest
+docker push leslykqi/multi-server:latest
+docker push leslykqi/multi-worker:latest
+docker push leslykqi/multi-client:$SHA
+docker push leslykqi/multi-server:$SHA
+docker push leslykqi/multi-worker:$SHA
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=leslykqi89/multi-server:$SHA
+kubectl set image deployments/server-deployment server=leslykqi/multi-server:$SHA
 kubectl set image deployments/client-deployment client=stephengrider/multi-client:latest
-kubectl set image deployments/worker-deployment worker=leslykqi89/multi-worker:$SHA
+kubectl set image deployments/worker-deployment worker=leslykqi/multi-worker:$SHA
